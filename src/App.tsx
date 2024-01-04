b473631f24f8a96daf9a700ae5a8086ece6012be
gward35 <g.g.ward90@gmail.com>
2024-01-03 20:52:20 -0500

    cleanuo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
 import './App.css'
 import { useState } from 'react'
 
-const config = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+const config = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1],
+]
 export interface ButtonProps {
   filled: boolean
   isDeactivating: boolean
@@ -10,9 +13,20 @@ export interface ButtonProps {
 }
 
 export const Button = ({ filled, isDeactivating, onClick }: ButtonProps) => {
-
-  return(
-    <button disabled={filled} style={{ 'backgroundColor': filled ? 'green' : 'white', border: '1px solid', borderRadius: 0, borderColor: '#fff', width: 'auto', height: 'auto', padding: '50px' }}  onClick={onClick}></button>
+  return (
+    <button
+      disabled={filled || isDeactivating}
+      style={{
+        backgroundColor: filled ? 'green' : 'white',
+        border: '1px solid',
+        borderRadius: 0,
+        borderColor: '#fff',
+        width: 'auto',
+        height: 'auto',
+        padding: '50px',
+      }}
+      onClick={onClick}
+    />
   )
 }
 
@@ -23,37 +37,50 @@ function App() {
   const deactivate = () => {
     setIsDeactivating(true)
     const timer = setInterval(() => {
-
       setOrder(prevState => {
         const newOrder = [...prevState]
         newOrder.pop()
 
-        if(newOrder.length === 0) {
+        if (newOrder.length === 0) {
           clearInterval(timer)
           setIsDeactivating(false)
         }
 
         return newOrder
       })
-
     }, 500)
-    console.log('d')
   }
 
   return (
     <>
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: "15px" }}>
-      {config.flat(1).map((c, i) => c ? <Button key={crypto.randomUUID()} isDeactivating={isDeactivating} filled={order.includes(i)} onClick={() => {
-        const newOrder = [...order, i]
-        setOrder(newOrder)
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gridGap: '15px',
+        }}
+      >
+        {config.flat(1).map((c, i) =>
+          c ? (
+            <Button
+              key={crypto.randomUUID()}
+              isDeactivating={isDeactivating}
+              filled={order.includes(i)}
+              onClick={() => {
+                const newOrder = [...order, i]
+                setOrder(newOrder)
 
-        if(newOrder.length === config.flat(1).filter(Boolean).length) {
-          deactivate()
-        }
-      }} /> : <span key={crypto.randomUUID()} />)}
-      
-    </div>
-    <p>{order.join(', ')}</p>
+                if (newOrder.length === config.flat(1).filter(Boolean).length) {
+                  deactivate()
+                }
+              }}
+            />
+          ) : (
+            <span key={crypto.randomUUID()} />
+          )
+        )}
+      </div>
+      <p>{order.join(', ')}</p>
     </>
   )
 }

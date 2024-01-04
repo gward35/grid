f2839ff0ddff4de8f9fab21376fdd3b751f98ac3
gward35 <g.g.ward90@gmail.com>
2024-01-03 20:49:50 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+
+import './App.css'
+import { useState } from 'react'
+
+const config = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+export interface ButtonProps {
+  filled: boolean
+  isDeactivating: boolean
+  onClick: (x: any) => void
+}
+
+export const Button = ({ filled, isDeactivating, onClick }: ButtonProps) => {
+
+  return(
+    <button disabled={filled} style={{ 'backgroundColor': filled ? 'green' : 'white', border: '1px solid', borderRadius: 0, borderColor: '#fff', width: 'auto', height: 'auto', padding: '50px' }}  onClick={onClick}></button>
+  )
+}
+
+function App() {
+  const [order, setOrder] = useState<number[]>([])
+  const [isDeactivating, setIsDeactivating] = useState<boolean>(false)
+
+  const deactivate = () => {
+    setIsDeactivating(true)
+    const timer = setInterval(() => {
+
+      setOrder(prevState => {
+        const newOrder = [...prevState]
+        newOrder.pop()
+
+        if(newOrder.length === 0) {
+          clearInterval(timer)
+          setIsDeactivating(false)
+        }
+
+        return newOrder
+      })
+
+    }, 500)
+    console.log('d')
+  }
+
+  return (
+    <>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: "15px" }}>
+      {config.flat(1).map((c, i) => c ? <Button key={crypto.randomUUID()} isDeactivating={isDeactivating} filled={order.includes(i)} onClick={() => {
+        const newOrder = [...order, i]
+        setOrder(newOrder)
+
+        if(newOrder.length === config.flat(1).filter(Boolean).length) {
+          deactivate()
+        }
+      }} /> : <span key={crypto.randomUUID()} />)}
+      
+    </div>
+    <p>{order.join(', ')}</p>
+    </>
+  )
+}
+
+export default App
